4245ac2e8a1ed33cc2efc049f4d0c1e96f13d8f7
timothy <tabradford@liberty.edu>
2017-04-12 11:46:23 -0400

    Initial commit

---
+++ src/sortalgorithms/Evaluator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sortalgorithms;
+
+/**
+ *
+ * @author ^_^
+ */
+public class Evaluator {
+
+    public static int selectionSortBradford() {
+        return -1;
+    }
+
+    public static int insertionSortBradford() {
+        return -1;
+    }
+
+    public static int mergeSortBradford() {
+        return -1;
+    }
+}
